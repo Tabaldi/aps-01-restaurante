c9837f05968537ce5e7a24a36df2f2e7a52be0fd
Michele Tabaldi <mptabaldi@gmail.com>
2017-09-12 23:32:06 -0300

    classe prato

---
+++ src/Prato.java
@@ -0,0 +1,50 @@
+
+public class Prato {
+	
+	//um nome, um preço, e uma descrição.
+	
+	private String name;
+	private Double preco;
+	private String descricao;
+	
+	public Prato(String name, Double preco){
+		super(); 
+		this.name = name;
+		this.preco = preco;
+	}
+	
+	public Prato(String name, Double preco, String descricao){
+		super(); 
+		this.name = name;
+		this.preco = preco;
+		this.descricao = descricao;
+	}
+	
+	public Prato(String name){
+		super(); 
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Double getPreco() {
+		return preco;
+	}
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+
+	@Override
+	public String toString() {
+		return "Prato [name=" + name + ", preco=" + preco + ", descricao=" + descricao + "]";
+	}
+	
+	
+}
