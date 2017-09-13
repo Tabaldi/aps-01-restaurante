42320a7a54407fde5283341364194ec55ba42ea9
Michele Tabaldi <mptabaldi@gmail.com>
2017-09-13 00:25:34 -0300

    Troca para array

--- src/Restaurante.java
+++ src/Restaurante.java
@@ -1,51 +1,22 @@
 
 public class Restaurante {
 
-	private Prato p1;
-	private Prato p2;
-	private Prato p3;
+	private Prato[] pratos = new Prato[3];
+
 	
-	public Restaurante(Prato p1, Prato p2, Prato p3){
+	public Restaurante(Prato[] pratos){
 		super();
-		this.p1 = p1;
-		this.p2 = p2;
-		this.p3 = p3;
+		this.pratos = pratos;
 	}
-	
 	
 	public void mostrarCardapio(){
-		System.out.println("Nome do Prato: "+p1.getName());
-		System.out.println("Descrição do Prato: "+p1.getDescricao());
-		
-		System.out.println("Nome do Prato: "+p2.getName());
-		System.out.println("Descrição do Prato: "+p2.getDescricao());
-		
-		System.out.println("Nome do Prato: "+p3.getName());
-		System.out.println("Descrição do Prato: "+p3.getDescricao());
+		this.imprimePratos(this.pratos);
 	}
-
-	public Prato getP1() {
-		return p1;
+	
+	private void imprimePratos(Prato[] pratos){
+		for (int i = 0; i < pratos.length; i++) {
+			 System.out.println("Nome do Prato: "+pratos[i].getName());
+			 System.out.println("Descrição do Prato: "+pratos[i].getDescricao());
+			}
 	}
-
-	public void setP1(Prato p1) {
-		this.p1 = p1;
-	}
-
-	public Prato getP2() {
-		return p2;
-	}
-
-	public void setP2(Prato p2) {
-		this.p2 = p2;
-	}
-
-	public Prato getP3() {
-		return p3;
-	}
-
-	public void setP3(Prato p3) {
-		this.p3 = p3;
-	}
-
 }
--- src/SistemaDePedidos.java
+++ src/SistemaDePedidos.java
@@ -5,8 +5,9 @@ public class SistemaDePedidos {
 		Prato p1 = new Prato("Xis S", 1.5D, "Xis Salada");
 		Prato p2 = new Prato("Xis SS", 1.5D, "Xis Super Salada");
 		Prato p3 = new Prato("Xis V", 1.4D, "Xis Vento");
+		Prato[] pratos = {p1,p2,p3};
 
-		Restaurante equilibrium = new Restaurante(p1, p2, p3);
+		Restaurante equilibrium = new Restaurante(pratos);
 		equilibrium.mostrarCardapio();
 	}
 
